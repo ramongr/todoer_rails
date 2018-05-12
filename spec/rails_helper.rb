d10dcc388f6fa05588c8735a90b4d1a4d7066973
Ramon Rodrigues <cerberus.ramon@gmail.com>
2018-05-12 15:52:15 +0100

    TD1 - helper file small fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __DIR__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
@@ -11,10 +12,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
 end
